ae7e8580e1e9236168a7a59416d5ec6df2c4f965
repitile <cecoisie@abv.bg>
2016-10-26 22:21:02 +0300

    Update 01_domashno.c

--- G/03/22/01_domashno.c
+++ G/03/22/01_domashno.c
@@ -5,19 +5,22 @@ int main()
 	float avr;
 
 	scanf("%d",&n);
-	
-	sum = 0, avr = 0;
 
-	for(i=0;i<=n;i++){
+	 sum = 0;
+	 avr = 0;
+
+	for(i=1;i<=n;i++)
+        {
 		scanf("%d",&a);
 		sum = sum  + a;
-		avr = sum; 
+		avr = sum;
 }
 
-avr=avr/n;
+avr=(float)avr/n;
 printf("Sum: %d\n",sum);
 printf("Avg: %.2f\n",avr);
 
 return 0;
+}
 
 
